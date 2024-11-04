27bec60e35c8f50aa6412c6c8a335a6cb52055b8
Colin Wu <85907102+ColinWu0403@users.noreply.github.com>
2024-11-04 10:27:19 -0500

    Updated urls.py

--- weatherAPI/urls.py
+++ weatherAPI/urls.py
@@ -9,6 +9,6 @@ urlpatterns = [
     path('weather/current/<str:location>/', views.get_weather_current, name='get_weather_current'),
     path('weather/history/<str:location>,<str:dt>/', views.get_weather_history, name='get_weather_history'),
     path('weather/forecast/<str:location>,<str:days>/', views.get_weather_forecast, name='get_weather_forecast'),
-    path('nws/forecast/<str:latitude>,<str:longitude>/', views.get_nws_forecast, name='nws_weather_forecast'),
-    path('nws/forecastHourly/<str:latitude>,<str:longitude>/', views.get_nws_hourly_forecast, name='nws_weather_hourly_forecast'),
+    path('nws/forecast/<str:location>/', views.get_nws_forecast, name='nws_weather_forecast'),
+    path('nws/forecastHourly/<str:location>/', views.get_nws_hourly_forecast, name='nws_weather_hourly_forecast'),
 ]
